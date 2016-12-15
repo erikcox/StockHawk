634d050820afbcdcb678da73f9d841b87f567566
Erik Cox <erikbcox@gmail.com>
2016-12-15 13:30:34 -0800

    Add comments to Utils

--- app/src/main/java/rocks/ecox/stockhawk/sync/Utils.java
+++ app/src/main/java/rocks/ecox/stockhawk/sync/Utils.java
@@ -10,16 +10,17 @@ import java.util.Locale;
 
 public class Utils {
 
+    // Create a query for stock history depending on symbol and date range
     public static String buildStockHistoryQuery(String symbol, String startDate, String endDate){
         return "select * from yahoo.finance.historicaldata where symbol = \'" +
                 symbol + "\' and startDate = \'" + startDate + "\' and endDate = \'" + endDate + "\'";
     }
 
+    // Grab the date range
     public static String getDate(int daysAgo){
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
         Calendar calendar = Calendar.getInstance();
         calendar.add(Calendar.DATE, -daysAgo);
         return dateFormat.format(calendar.getTime());
     }
-
 }
