3856b5a9c5f1b6872fac9eac5761e00b698bf2e6
Erik Cox <erikbcox@gmail.com>
2016-12-15 11:53:03 -0800

    Add widget functionality

--- app/src/main/java/rocks/ecox/stockhawk/data/HistoricalQuoteColumns.java
+++ app/src/main/java/rocks/ecox/stockhawk/data/HistoricalQuoteColumns.java
@@ -14,22 +14,22 @@ public interface HistoricalQuoteColumns {
     @DataType(DataType.Type.INTEGER)
     @PrimaryKey
     @AutoIncrement
-    public static final String _ID =
+    String _ID =
             "_id";
 
     @DataType(DataType.Type.TEXT)
     @NotNull
-    public static final String SYMBOL = "symbol";
+    String SYMBOL = "symbol";
 
     @DataType(DataType.Type.TEXT)
     @NotNull
-    public static final String DATE = "date";
+    String DATE = "date";
 
     @DataType(DataType.Type.TEXT)
     @NotNull
-    public static final String OPEN = "open";
+    String OPEN = "open";
 
     @DataType(DataType.Type.TEXT)
     @NotNull
-    public static final String CLOSE = "close";
+    String CLOSE = "close";
 }
