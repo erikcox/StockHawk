1d02a8bd55bf1134ef0dc8fdffd57f5c2bc6bec0
Erik Cox <erikbcox@gmail.com>
2016-12-15 13:28:20 -0800

    Add comments to DetailActivity

--- app/src/main/java/rocks/ecox/stockhawk/ui/DetailActivity.java
+++ app/src/main/java/rocks/ecox/stockhawk/ui/DetailActivity.java
@@ -17,6 +17,7 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        // Replace placeholder with DetailFragment instance
         if (savedInstanceState == null){
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.detail_container, new DetailFragment())
